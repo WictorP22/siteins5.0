b4222e2a0ec2a6888af3025b7a4ba2d259264581
WictorP22 <wictorpamp@gmail.com>
2023-05-28 13:49:24 -0300

    moment

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import time
 from flask import Flask, render_template, request, jsonify, json
 import gspread, json, requests
 from datetime import datetime
-from flask_moment import Moment
 from lista_membros import *
 from blogger import *
 
