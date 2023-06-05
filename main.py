a6a0a067dbf6aa9bc7fc01a3b948be946d703cb7
WictorP22 <wictorpamp@gmail.com>
2023-06-05 03:50:15 -0300

    coloca login

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import time
 
-from flask import Flask, render_template, request, jsonify, json
+from flask import Flask, render_template, request, jsonify, json, session, redirect
 import gspread, json, requests
 from datetime import datetime
 from flask_moment import Moment
 from lista_membros import *
+from db import *
 from blogger import *
+import mysql.connector
+from mysql.connector import errorcode
+from flask_bcrypt import Bcrypt
+from random import *
 
-
-
+db_connection = mysql.connector.connect(host='us-cdbr-east-06.cleardb.net', user='b5010672adeb65', password='b5d9fcae', database='heroku_40a73087811b6c2')
+cursor = db_connection.cursor()
 #gc = gspread.service_account(filename='service_acoount.json')
 #sh = gc.open_by_key('1SkdGGG_ke6rE_-PACEw8EIr5PeHEPRh-t0fnp46PX88')
 #worksheet = sh.worksheet("Página120")
 #valor = worksheet.get('A1:D3')
-
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+bcrypt = Bcrypt(app)
 moment = Moment()
 moment.init_app(app)
 # criar a 1ª página
@@ -27,30 +33,112 @@ def relatorios():
     respostas = relatoriosEst.get('B6:I')
     return jsonify(respostas)
 
+#login
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if 'logado' in session:
+        return redirect('/')
+    if request.method == 'POST':
+        enviou = logar(request.form['nick'], request.form['senha'])
+        if enviou['resultado'] == 'false':
+            session['logado'] = True
+            session['nick'] = request.form['nick']
+            return redirect('/')
+        else:
+            return render_template("login/login.html", enviou=enviou['msg'])
+    else:
+        enviou="Preencha os dados para acessar o sistema"
+        return render_template("login/login.html", enviou=enviou)
+
+#registro
+@app.route("/registro", methods=['GET', 'POST'])
+def registro():
+    if 'logado' in session:
+        return redirect('/')
+    if request.method == 'POST':
+        enviou = registrar(request.form['nick'], request.form['tag'], request.form['email'], request.form['senha'], request.form['confirmasenha'], session['codigo'])
+        return render_template("login/register.html", enviou=enviou, codigo=session['codigo'])
+    else:
+        aleatorio = str(randint(0, 100))
+        motto = "Ativar-INS" + aleatorio
+        session['codigo'] = motto
+        enviou="Preencha os dados para registrar"
+    return render_template("login/register.html", enviou=enviou, codigo=session['codigo'])
+
+#recuperar
+@app.route("/recuperar", methods=['GET', 'POST'])
+def recuperar():
+    if 'logado' in session:
+        return redirect('/')
+    if request.method == 'POST':
+        enviou = recuperando(request.form['nick'], session['codigo'], request.form['senha'], request.form['confirmasenha'])
+        return render_template("login/recuperar.html", enviou=enviou, codigo=session['codigo'])
+    else:
+        aleatorio = str(randint(0, 100))
+        motto = "Recuperar-INS" + aleatorio
+        session['codigo'] = motto
+        enviou = "Você esqueceu sua senha? Aqui você pode facilmente recuperar uma nova senha"
+        return render_template("login/recuperar.html", enviou=enviou, codigo=session['codigo'])
+
+@app.route("/convidado", methods=['GET', 'POST'])
+def convidado():
+    if 'logado' in session:
+        return redirect('/')
+    if request.method == 'POST':
+        nick = request.form['nick']
+        usuario = requests.get(f"https://www.habbo.com.br/api/public/users?name={nick}")
+        membro = json.loads(usuario.content)
+        if not 'error' in membro:
+            session['logado'] = True
+            session['nick'] = nick
+            return redirect("/")
+        else:
+            return render_template("login/convidado.html", enviou="<span class='text-danger'>Não é um Habbo válido.</span>")
+    else:
+        return render_template("login/convidado.html", enviou="Insira seu nick para entrar como convidado")
+
+#logou
+@app.route("/logout")
+def logout():
+    if 'logado' in session:
+        session.pop('nick', None)
+        session.pop('logado', None)
+    return redirect('/login')
+
 #index
 @app.route("/")
 def index():
+    if not 'logado' in session:
+        return redirect('/login')
     return render_template("indexmeu.html", podio=busca_podio())
 
 #documentos
 @app.route("/documentos")
 def documentos():
+    if not 'logado' in session:
+        return redirect('/login')
     return render_template("docs.html", regimento=busca_anuncio('3073735437882198575'), penal=busca_anuncio('2651698189980401399'), apostilas=busca_apostilas())
 
 #anuncios
 @app.route("/anuncios")
 def anuncios():
+    if not 'logado' in session:
+        return redirect('/login')
     return render_template("anuncios.html", anuncios=anuncios_blogger())
 
 #anuncio
 @app.route("/anuncio/<id>")
 def anuncio(id):
+    if not 'logado' in session:
+        return redirect('/login')
     return render_template("anuncio.html", anuncio=busca_anuncio(id))
 
 
 #url variavel
 @app.route("/instrutor")
 def instrutores():
+    if not 'logado' in session:
+        return redirect('/login')
     instrutor = request.args.get('policial')
     if instrutor == '':
         return render_template("indexmeu.html")
@@ -59,15 +147,21 @@ def instrutores():
 
 @app.route("/projetos")
 def projetos():
+    if not 'logado' in session:
+        return redirect('/login')
     return render_template("projetos.html", projetos=lista_projetos())
 
 @app.route("/membros")
 def membros():
+    if not 'logado' in session:
+        return redirect('/login')
     return render_template("membros.html", lista=lista_membro())
 
-@app.route("/politica")
-def politica():
-    return render_template("policia.html")
+@app.route("/auditoria")
+def auditoria():
+    if not 'logado' in session:
+        return redirect('/login')
+    return render_template("auditoria.html", mes=busca_auditoria('2023'))
 
 #consultar instrutor
 def consulta_instrutor(id):
---
+++ static/db.py
@@ -0,0 +1,111 @@
+from main import bcrypt
+import mysql.connector, requests, json
+from lista_membros import *
+from flask_bcrypt import Bcrypt
+
+def db():
+	return mysql.connector.connect(host='us-cdbr-east-06.cleardb.net', user='b5010672adeb65', password='b5d9fcae', database='heroku_40a73087811b6c2')
+
+def registrar(nick, tag, email, senha, confirmasenha, missao):
+	if checainfo('nick', nick) == 'true':
+		return "<span class='text-danger'>Nick já em uso</span>"
+	if checainfo('email', email) == 'true':
+		return "<span class='text-danger'>Email já em uso</span>"
+	if checainfo('tag', tag) == 'true':
+		return "<span class='text-danger'>TAG já em uso</span>"
+	if senha != confirmasenha:
+		return "<span class='text-danger'>Senhas não coincidem</span>"
+	usuario = requests.get(f"https://www.habbo.com.br/api/public/users?name={nick}")
+	membro = json.loads(usuario.content)
+	if membro['motto'] != missao:
+		return "<span class='text-danger'>Insira o código abaixo na missão do seu avatar</span>"
+	plan = busca_lista(nick)
+	if plan['Cargo'] == "-":
+		return "<span class='text-danger'>Usuário não ativo na lista de membros</span>"
+	senhaEncript = bcrypt.generate_password_hash(senha)
+	sql = "INSERT INTO usuarios (nick, email, tag, senha, situacao) VALUES(%(nick)s, %(email)s, %(tag)s, %(senha)s, 'A')"
+	nzc = db()
+	cursor = nzc.cursor()
+	cursor.execute(sql, {
+		'nick': nick,
+		'email': email,
+		'tag': tag,
+		'senha': senhaEncript
+	})
+	nzc.commit()
+	cursor.reset()
+	cursor.close()
+	return"<span class='text-success'>Usuário cadastrado e ativo</span>"
+
+def logar(nick, senha2):
+	nzc = db()
+	cursor = nzc.cursor()
+	sql = f"SELECT id, nick, senha FROM usuarios WHERE nick=%(nome)s"
+	cursor.execute(sql, {'nome': nick})
+	quantidade = 0
+	for(id,nick, senha) in cursor:
+		quantidade = cursor.rowcount
+		novaSenha = senha
+	cursor.reset()
+	cursor.close()
+	if quantidade == 1:
+		senhaEncript = bcrypt.check_password_hash(novaSenha, senha2)
+		if senhaEncript == True:
+			return {
+				'resultado': 'false',
+				'msg': "<span class='text-success'>Conectado com sucesso</span>"
+			}
+		else:
+			return {
+				'resultado': 'true',
+				'msg': "<span class='text-danger'>Senha incorreta</span>"
+			}
+	else:
+		return {
+			'resultado': 'true',
+			'msg': "<span class='text-danger'>Usuário não cadastrado no sistema</span>"
+		}
+
+def recuperando(nick, missao, senha, novasenha):
+	usuario = requests.get(f"https://www.habbo.com.br/api/public/users?name={nick}")
+	membro = json.loads(usuario.content)
+	if membro['motto'] != missao:
+		return "<span class='text-danger'>Insira o código abaixo na missão do seu avatar</span>"
+	plan = busca_lista(nick)
+	if plan['Cargo'] == "-":
+		return "<span class='text-danger'>Usuário não ativo na lista de membros</span>"
+	nzc = db()
+	cursor = nzc.cursor()
+	sql = f"SELECT id, nick, situacao FROM usuarios WHERE nick='{nick}'"
+	cursor.execute(sql)
+	quantidade = 0
+	for (id, nick, situacao) in cursor:
+		quantidade = cursor.rowcount
+		iduser = id
+	cursor.reset()
+	if quantidade == 1:
+		senhaEncript = bcrypt.generate_password_hash(senha)
+		sql = "UPDATE usuarios SET senha=%(senha)s, situacao='A' WHERE id=%(nick)s"
+		cursor.execute(sql, {'senha': senhaEncript, 'nick': iduser})
+		nzc.commit()
+		cursor.reset()
+		cursor.close()
+		return "<span class='text-success'>Trocou a senha</span>"
+	else:
+		return "<span class='text-danger'>Usuário não encontrado em nosso sistema</span>"
+
+
+def checainfo(coluna, valor):
+	nzc = db()
+	cursor = nzc.cursor()
+	sql = f"SELECT id, nick, email, tag FROM usuarios WHERE {coluna}='{valor}'"
+	cursor.execute(sql)
+	quantidade = 0
+	for (id, nick, email, tag) in cursor:
+		quantidade = cursor.rowcount
+	cursor.reset()
+	cursor.close()
+	if quantidade >= 1:
+		return 'true'
+	else:
+		return 'false'
